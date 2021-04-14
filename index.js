7678a70016d2c0029dd5124498720a2fb1bbdbb2
Andrehochi <andrehochiwork@gmail.com>
2021-04-14 14:09:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(array) {
+    let results = array.find(game => game.result == 'W')
+    try {
+        return results.year
+    } catch (err) {
+        return results
+    }
+}
